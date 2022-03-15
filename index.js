e3b650527e5559141988c2e3375f62690f2e32d8
Lyere <BPpearsall@gmail.com>
2022-03-15 18:56:18 -0400

    finished page-template html and ran full-stack successful tests

--- index.js
+++ index.js
@@ -11,21 +11,14 @@ const distPath = path.join(DIST_DIR, 'team.html');
 const render = require('./src/page-template.js');
 
 const teamMembers = [];
-// Create an id array to store the ids.
-// This array will be used to check the potential duplicate id newly entered by user
+
 const idArray = [];
 
-// Inform user of usage
 console.log(
   '\nWelcome to the team generator!\nUse `npm run reset` to reset the dist/ folder\n'
 );
 
 function appMenu() {
-  // Create manager first, then the manager will create a team
-  // Once manager is craeted, we will create team by asking the user which type of employee to create
-  // Based on the choice, we will create that employee object
-  // Loop throu the create team function until user is done from creating employees for the team
-  // then we will use the employee objects created to render html for the team
 
   function createManager() {
     console.log('Please build your team 👥');
@@ -42,11 +35,6 @@ function appMenu() {
             return 'Please enter at least one character.';
           },
         },
-        //
-        // TODO: YOUR CODE HERE:
-        // CREATE REST OF QUESTIONS HERE FOR MANAGER
-        // Strongly recommend to add validate property function for id and email
-        //
       ])
       .then((answers) => {
         const manager = new Manager(
@@ -93,66 +81,52 @@ function appMenu() {
     inquirer
       .prompt([{
         type: 'input',
-        name: 'gitHub',
+        name: 'github',
         message: 'What is your GitHub profile name?'
       }
-        //
-        // TODO: YOUR CODE HERE
-        // CREATE OBJECTS OF QUESTIONS FOR ENGINEER
-        //
       ])
       .then((answers) => {
         const engineer = new Engineer(
           answers.engineerName,
           answers.engineerId,
           answers.engineerEmail,
-          answers.engineergitHub
+          answers.engineergithub
         );
         teamMembers.push(engineer);
         idArray.push(answers.engineerId);
         createTeam();
       });
-    //
-    // TODO: YOUR CYOUR CODE HERE
-    // 1. CREATE A VARIABLE TO STORE THE ENGINEER OBJECT INSTANTIATED WITH THE ENGINEER CLASS, PASSING ANSWERS PROPERTIES AS INPUT AURGUMENTS
-    //    TO THE ENGINEER CLASS CONSTRUCTOR
-    // 2. ADD (PUSH) THE ENGINEER VARIABLE TO the teamMembers ARRAY
-    // 3. ADD (PUSH) THE ENGINERR ID TO THE idArray ARRAY
-    //
-
-    createTeam();
-  });
-}
-
-function addIntern() {
-  inquirer
-    .prompt([
-      //
-      // TODO: YOUR CODE HERE
-      // CREATE OBJECTS OF QUESTIONS FOR INTERN
-      //
-    ])
-    .then((answers) => {
-      //
-      // TODO: YOUR CODE HERE
-      // 1. CREATE A VARIABLE TO STORE THE INTERN OBJECT INSTANTIATED WITH THE INTERN CLASS, PASSING ANSWERS PROPERTIES AS INPUT AURGUMENTS
-      //    TO THE INTERN CLASS CONSTRUCTOR
-      // 2. ADD (PUSH) THE INTERN VARIABLE TO the teamMembers ARRAY
-      // 3. ADD (PUSH) THE INTERN ID TO THE idArray ARRAY
-      //
-      createTeam();
-    });
-}
-
-function buildTeam() {
-  // Create the output directory if the dist path doesn't exist
-  if (!fs.existsSync(DIST_DIR)) {
-    fs.mkdirSync(DIST_DIR);
   }
-  fs.writeFileSync(distPath, render(teamMembers), 'utf-8');
-}
 
-createManager();
+  function addIntern() {
+    inquirer
+      .prompt([{
+        type: 'input',
+        name: 'school',
+        message: 'What school did you go to'
+      }
+      ])
+      .then((answers) => {
+        const intern = new Intern(
+          answers.internName,
+          answers.internId,
+          answers.internEmail,
+          answers.internSchool
+        );
+        teamMembers.push(intern);
+        idArray.push(answers.internId);
+        createTeam();
+      });
+  }
+
+  function buildTeam() {
+    if (!fs.existsSync(DIST_DIR)) {
+      fs.mkdirSync(DIST_DIR);
+    }
+    fs.writeFileSync(distPath, render(teamMembers), 'utf-8');
+  }
+
+  createManager();
 }
 
 appMenu();
