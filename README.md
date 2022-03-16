134ec6cbc9010f51329e931dedae106123d3f72d
Lyere <BPpearsall@gmail.com>
2022-03-16 09:56:28 -0400

    added links to template html

---
+++ README.md
@@ -0,0 +1,8 @@
+# Professional Team Generator
+
+## Description
+In this program I want to be able to dynamically create cards that will house basic information about my team's manager, engineers and interns.
+
+## Process
+Through the usage of Node and Inquirer, I ask the user multiple prompts in the terminal, and their answers are logged and will dynamically create and HTML page with all the relevant information.
+
--- src/page-template.js
+++ src/page-template.js
@@ -32,7 +32,7 @@ const generateTeam = team => {
             <ul class="list-group">
                 <li class="list-group-item">ID: ${engineer.getId()}</li>
                 <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
-                <li class="list-group-item">GitHub Profile: ${engineer.getGithub()}</li>
+                <li class="list-group-item">GitHub Profile: https://github.com/${engineer.getGithub()}</li>
             </ul>
         </div>
     </div>
